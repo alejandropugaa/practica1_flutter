b29ec305dfb78a4c5d39c83f5bb97adb5f6ccde8
Alejandro Puga <21030634@itcelaya.edu.mx>
2025-09-06 12:05:04 -0600

    Cambio de imagenes y datos

--- lib/hero_model.dart
+++ lib/hero_model.dart
@@ -14,24 +14,24 @@ class HeroModel {
 
 final List<HeroModel> heroes = [
   HeroModel(
-    name: 'Bar Bar Green',
-    image: 'assets/player1.png',
-    speed: 16.0,
-    health: 40.0,
+    name: 'Bajaj N250',
+    image: 'assets/n250.png',
+    speed: 60.0,
+    health: 63.0,
     attack: 65.0,
   ),
   HeroModel(
-    name: 'Cow Master',
-    image: 'assets/player2.png',
-    speed: 25.0,
-    health: 50.0,
-    attack: 75.0,
+    name: 'Honda CB190R',
+    image: 'assets/cb190r.png',
+    speed: 52.0,
+    health: 59.0,
+    attack: 66.0,
   ),
   HeroModel(
-    name: 'Bombardier',
-    image: 'assets/player3.png',
-    speed: 10.0,
-    health: 80.0,
-    attack: 80.0,
+    name: 'CFMoto 250NK',
+    image: 'assets/250nk.png',
+    speed: 68.0,
+    health: 75.0,
+    attack: 70.0,
   ),
 ];
